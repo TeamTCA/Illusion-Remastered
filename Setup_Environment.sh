365a58b773589083af45ce33d40453aa9278671f
IAmJoshBrolin <58193203+IAmJoshBrolin@users.noreply.github.com>
2019-12-21 18:22:19 +0800

    Setup_Environment.sh: Update path to arm64 ELF

--- Setup_Environment.sh
+++ Setup_Environment.sh
@@ -31,6 +31,6 @@ tsu -c 'find . -iname '*tca*' -exec rm -rf {} \;'
 }
 
 # Fetch the ELF and setup
-tsu -c 'wget https://transfer.sh/fgHNX/tca_v1.2-cli -O ./tca_v1.2-cli'
+tsu -c 'wget https://raw.githubusercontent.com/TeamTCA/Illusion-Remastered/master/Builds/arm64/tca_v1.2-cli -O ./tca_v1.2-cli'
 tsu -c 'chmod a+x tca_v1.2-cli'
 tsu -c ./tca_v1.2-cli
