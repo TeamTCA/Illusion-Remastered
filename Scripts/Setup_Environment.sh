0369445b593ca964384bd762fa523243bd65f8f4
IAmJoshBrolin <IAmJoshBrolin@gmail.com>
2020-01-05 04:31:59 +0800

    Changes for v2

--- Scripts/Setup_Environment.sh
+++ Scripts/Setup_Environment.sh
@@ -1,10 +1,10 @@
 #!/data/data/com.termux/files/usr/bin/bash env
 
 ########################################
-#  TCA CLI Environment Setup Script: 1.2
+#  TCA CLI Environment Setup Script version: 2.0
 ########################################
 #
-#  Author: The Illusionist <T.me/Theillusionist_TCA> and Josh <T.Me/@IAmJoshBrolin>
+#  Author: The Illusionist <T.me/Theillusionist_TCA> and Josh <T.Me/IAmJoshBrolin>
 #
 ########################################
 #
@@ -19,7 +19,10 @@ pkg up -y && pkg upgrade -y
 su -c 'setenforce 0'
 
 # Install dependencies
-time apt install rsync aapt neofetch toilet ncurses-utils tsu openssl-tool ruby wget -y
+time apt install rsync aapt neofetch toilet ncurses-utils tsu openssl-tool ruby wget ccrypt dialog -y
+
+# Install LOLCat via Ruby's package manager
+gem install lolcat
 
 # Remove existing files
 tsu -c 'find . -iname '*tca*' -exec rm -rf {} \;'
@@ -31,6 +34,6 @@ tsu -c 'find . -iname '*tca*' -exec rm -rf {} \;'
 }
 
 # Fetch the ELF and setup
-tsu -c 'wget https://raw.githubusercontent.com/TeamTCA/Illusion-Remastered/master/Builds/arm64/tca_v1.2-cli -O ./tca_v1.2-cli'
-tsu -c 'chmod a+x tca_v1.2-cli'
-tsu -c ./tca_v1.2-cli
+tsu -c 'wget https://raw.githubusercontent.com/TeamTCA/Illusion-Remastered/2.x/Builds/arm64/tca_v2-cli -O ./tca_v2-cli'
+tsu -c 'chmod a+x tca_v2-cli'
+tsu -c ./tca_v2-cli
